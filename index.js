595416e9843c62898c7103599e3a096150cfba89
harshaVarnam <132270561+harshaVarnam@users.noreply.github.com>
2023-06-11 18:58:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function makeSound(key){
             crash.play();
             break;
         case "a":
-            let kick = new Audio("sounds/Kick-bass.mp3");
+            let kick = new Audio("sounds/kick-bass.mp3");
             kick.play();
             break;
         case "s":
